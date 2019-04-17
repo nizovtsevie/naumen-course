e5630347911b38c269abedb2e87911f366a19ae7
Inlians <vladislav-sivirukhin@yandex.ru>
2019-04-17 11:00:04 +0500

    Delete naumen-server folder

--- naumen-server/src/main/java/com/server/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.server;
-
-import com.server.Entities.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends CrudRepository<User, Long> {
-}
